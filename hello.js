0c862bc97bee156ad42aca2a74bc707866d4ea02
tlynchkc1 <tlynchkc@gmail.com>
2021-05-27 22:21:52 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+	let message = "Hello World!";
+	return message;
 }
  
 module.exports = hello;
